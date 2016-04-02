dfb77ec80dae1c7e2f6de8d4741e97c1f4b9ef0e
Samuel Ting <samueltingko@gmail.com>
2016-04-02 22:23:12 +0800

    integration

--- src/app.php
+++ src/app.php
@@ -1,10 +1,12 @@
+
 <?php
 
 /*
 $app->register(new Acme\DatabaseServiceProvider());
 
  */
+$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__.'/views',
+        'twig.path' => __DIR__.'/../web/views',
     ));
-$app['asset_path'] = 'http://127.0.0.1/~samuelting/100in1day/Resources';
+$app['asset_path'] = 'http://127.0.0.1/~samuelting/100in1day/resources';
