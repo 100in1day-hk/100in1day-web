c777666d1c1695adebee437b3ca48d010731ff4c
Samuel Ting <samueltingko@gmail.com>
2016-04-05 23:28:31 +0800

    updated support display

--- src/app.php
+++ src/app.php
@@ -6,6 +6,9 @@ $app->register(new Acme\DatabaseServiceProvider());
 
  */
 $app['debug'] = true;
+$app['server_url'] = 'http://127.0.0.1/~samuelting';
+
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../web/views',
     ));
@@ -22,4 +25,6 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     ),
 ));
 
-$app['asset_path'] = 'http://127.0.0.1/~samuelting/100in1day/resources';
+$app['asset_path'] = $app['server_url'].'/100in1day/resources';
+$app['upload_path'] = $app['server_url'].'/upload';
+
